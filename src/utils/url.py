0758c6d51a4c29f772ad6c2d81e371ab9f75bc96
Martin Minkov <minkovlmartin@gmail.com>
2023-08-26 16:02:47 -0700

    feat(vscode/settings.json): change python formatting provider from none to autopep8 for better code formatting
    feat(utils/url.py): add default port assignment based on URL scheme for better URL handling
    
    fix(utils/url.py): modify socket connection to use assigned port instead of hard-coded value for better flexibility and correctness

--- src/utils/url.py
+++ src/utils/url.py
@@ -6,6 +6,12 @@ class URL:
     def __init__(self, url: str):
         self.scheme, url = url.split("://", 1)
         assert self.scheme in ["http", "https"], "Invalid URL scheme"
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
         if "/" not in url:
             url += "/"
         self.host, url = url.split("/", 1)
@@ -15,19 +21,24 @@ class URL:
         s = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
-        ctx = ssl.create_default_context()
-        s = ctx.wrap_socket(s, server_hostname=self.host)
-        s.connect((self.host, 80))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
+        s.connect((self.host, self.port))
         s.send(
             (
                 "GET {} HTTP/1.0\r\n".format(self.path)
                 + "Host: {}\r\n\r\n".format(self.host)
             ).encode("utf8")
         )
+
         response = s.makefile("r", encoding="utf8", newline="\r\n")
         statusline = response.readline()
         version, status, explanation = statusline.split(" ", 2)
         assert status == "200", "{}: {}".format(status, explanation)
+
         headers = {}
         while True:
             line = response.readline()
@@ -37,6 +48,7 @@ class URL:
             headers[header.lower()] = value.strip()
             assert "transfer-encoding" not in headers
             assert "content-encoding" not in headers
+
         body = response.read()
         s.close()
         return headers, body
