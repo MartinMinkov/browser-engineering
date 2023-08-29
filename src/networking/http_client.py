8e59f81306cb96c7c673c746a8bf37ee09879415
Martin Minkov <minkovlmartin@gmail.com>
2023-08-29 16:48:49 -0700

    refactor(http_client.py): extract socket creation to a separate method for better readability and maintainability feat(http_client.py): add type hints to methods for better type checking and code understanding refactor(http_client.py): extract header reading to a separate method to improve code organization and readability fix(http_client.py): strip header and value to remove leading/trailing spaces when reading headers

--- src/networking/http_client.py
+++ src/networking/http_client.py
@@ -8,35 +8,49 @@ from src.utils.url import URL, Scheme
 
 
 class HTTPClient:
-    def __init__(self, url: URL, encoding="utf8"):
+    url: URL
+    encoding: str
+    s: socket.socket
+
+    def __init__(self, url: URL, encoding: str = "utf8"):
         self.url = url
         self.encoding = encoding
-        self.s = socket.socket(
+        self.s = self._create_socket()
+
+    def _create_socket(self) -> socket.socket:
+        s = socket.socket(
             family=socket.AF_INET, type=socket.SOCK_STREAM, proto=socket.IPPROTO_TCP
         )
-        if url.scheme == Scheme.HTTPS:
+        if self.url.scheme == Scheme.HTTPS:
             ctx = ssl.create_default_context()
-            self.s = ctx.wrap_socket(self.s, server_hostname=url.host)
+            s = ctx.wrap_socket(s, server_hostname=self.url.host)
+        return s
 
-    def send_request(self, request: Request):
+    def send_request(self, request: Request) -> Response:
         self.s.connect((self.url.host, self.url.port))
         self.s.send((str(request)).encode(self.encoding))
-        response = self.parse_request()
+        response = self._parse_request()
         self.s.close()
         return response
 
-    def parse_request(self):
-        response = self.s.makefile("r", encoding=self.encoding, newline="\r\n")
-        statusline = response.readline()
+    def _parse_request(self) -> Response:
+        response_file = self.s.makefile("r", encoding=self.encoding, newline="\r\n")
+        statusline = response_file.readline()
         version, status, explanation = statusline.split(" ", 2)
 
-        response_headers = Headers()
+        response_headers = self._read_headers(response_file)
+
+        response = Response(
+            int(status), explanation, response_headers, response_file.read()
+        )
+        return response
+
+    def _read_headers(self, response_file) -> Headers:
+        headers = Headers()
         while True:
-            line = response.readline()
+            line = response_file.readline()
             if line == "\r\n":
                 break
             header, value = line.split(":", 1)
-            response_headers.add_header(header, value)
-
-        response = Response(int(status), explanation, response_headers, response.read())
-        return response
+            headers.add_header(header.strip(), value.strip())
+        return headers
