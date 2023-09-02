79ab230dea2e174c35830526e540a5f2748cae80
Martin Minkov <minkovlmartin@gmail.com>
2023-09-02 12:13:03 -0700

    feat(browser.py): add display_list and scroll attributes to Browser class to support scrolling refactor(browser.py): extract layout logic into a separate _layout method for better code organization feat(browser.py): add draw method to render display_list items on canvas, considering scroll value for better user experience

--- src/render/browser.py
+++ src/render/browser.py
@@ -1,4 +1,5 @@
 import tkinter
+from typing import List, Tuple
 
 from src.networking.cache import BrowserCache
 from src.utils.url import AbstractURL
@@ -7,28 +8,43 @@ from src.view.view_factory import ViewFactory
 WIDTH = 800
 HEIGHT = 600
 
+HSTEP = 13
+VSTEP = 18
+
 
 class Browser:
     window: tkinter.Tk
     canvas: tkinter.Canvas
     cache: BrowserCache
+    display_list: List[Tuple[str, int, int]]
+    scroll: int
 
     def __init__(self):
         self.window = tkinter.Tk()
         self.canvas = tkinter.Canvas(self.window, width=WIDTH, height=HEIGHT)
         self.canvas.pack()
         self.cache = BrowserCache()
+        self.display_list = []
+        self.scroll = 0
+
+    def _layout(self, text: str) -> List[Tuple[str, int, int]]:
+        display_list = []
+        cursor_x, cursor_y = HSTEP, VSTEP
+        for c in text:
+            display_list.append((c, cursor_x, cursor_y))
+            cursor_x += HSTEP
+            if cursor_x >= WIDTH - HSTEP:
+                cursor_x = HSTEP
+                cursor_y += VSTEP
+        return display_list
 
     def load(self, url: AbstractURL):
         view = ViewFactory.create(url)
         document = view.view_load(self.cache)
         body = view.lex(document)
+        self.display_list = self._layout(body)
+        self.draw()
 
-        HSTEP, VSTEP = 13, 18
-        cursor_x, cursor_y = HSTEP, VSTEP
-        for c in body:
-            self.canvas.create_text(cursor_x, cursor_y, text=c)
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_x = HSTEP
-                cursor_y += VSTEP
+    def draw(self):
+        for c, x, y in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
