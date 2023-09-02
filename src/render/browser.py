dd2e2c9af6ad05179cb43beda7ace2700650b20c
Martin Minkov <minkovlmartin@gmail.com>
2023-09-02 15:37:38 -0700

    feat(browser.py): add font size adjustment functionality to improve user experience feat(browser.py): add window close functionality for better control refactor(browser.py): move HSTEP and VSTEP into Browser class for better encapsulation fix(browser.py): adjust text rendering to use dynamic font size and steps for better layout consistency

--- src/render/browser.py
+++ src/render/browser.py
@@ -1,4 +1,5 @@
 import tkinter
+import tkinter.font
 from enum import Enum
 from typing import List, Tuple
 
@@ -9,10 +10,10 @@ from src.view.view_factory import ViewFactory
 WIDTH = 800
 HEIGHT = 600
 
-HSTEP = 13
-VSTEP = 18
 
 SCROLL_STEP = 100
+
+DEFUALT_FONT_SIZE = 12
 
 
 class WindowBindings(Enum):
@@ -24,6 +25,9 @@ class WindowBindings(Enum):
     SCROLL_UP = "<Button-4>"
     SCROLL_DOWN = "<Button-5>"
     RESIZE = "<Configure>"
+    PLUS = "<plus>"
+    MINUS = "<minus>"
+    CTRL_D = "<Control-d>"
 
     def __str__(self):
         return self.value
@@ -36,6 +40,9 @@ class Browser:
     display_list: List[Tuple[str, int, int]]
     scroll: int
     content: str
+    font: tkinter.font.Font
+    HSTEP = 13
+    VSTEP = 18
 
     def __init__(self):
         self.window = tkinter.Tk()
@@ -45,6 +52,7 @@ class Browser:
         self.display_list = []
         self.scroll = 0
         self.content = ""
+        self.font = tkinter.font.Font(size=DEFUALT_FONT_SIZE)
         self._init_window_bindings()
 
     def _init_window_bindings(self):
@@ -53,6 +61,27 @@ class Browser:
         self.window.bind(str(WindowBindings.SCROLL_DOWN), self._scroll_down)
         self.window.bind(str(WindowBindings.SCROLL_UP), self._scroll_up)
         self.window.bind(str(WindowBindings.RESIZE), self._resize)
+        self.window.bind(str(WindowBindings.PLUS), self._increase_font_size)
+        self.window.bind(str(WindowBindings.MINUS), self._decrease_font_size)
+        self.window.bind(str(WindowBindings.CTRL_D), self._close_window)
+        self.window.bind(str())
+
+    def _close_window(self, _: tkinter.Event):
+        self.window.destroy()
+
+    def _increase_font_size(self, _: tkinter.Event):
+        self.font = tkinter.font.Font(size=self.font.actual()["size"] + 1)
+        self.HSTEP = self.HSTEP + 2
+        self.VSTEP = self.font.metrics("linespace")
+        self.display_list = self._layout(self.content)
+        self.draw()
+
+    def _decrease_font_size(self, _: tkinter.Event):
+        self.font = tkinter.font.Font(size=self.font.actual()["size"] - 1)
+        self.HSTEP = self.HSTEP - 2
+        self.VSTEP = self.font.metrics("linespace")
+        self.display_list = self._layout(self.content)
+        self.draw()
 
     def _resize(self, event: tkinter.Event):
         global WIDTH, HEIGHT
@@ -75,16 +104,16 @@ class Browser:
 
     def _layout(self, text: str) -> List[Tuple[str, int, int]]:
         display_list = []
-        cursor_x, cursor_y = HSTEP, VSTEP
+        cursor_x, cursor_y = self.HSTEP, self.VSTEP
         for c in text:
             if c == "\n":
-                cursor_x = HSTEP
-                cursor_y += VSTEP
+                cursor_x = self.HSTEP
+                cursor_y += self.VSTEP
                 continue
-            cursor_x += HSTEP
-            if cursor_x >= WIDTH - HSTEP:
-                cursor_x = HSTEP
-                cursor_y += VSTEP
+            cursor_x += self.HSTEP
+            if cursor_x >= WIDTH - self.HSTEP:
+                cursor_x = self.HSTEP
+                cursor_y += self.VSTEP
             display_list.append((c, cursor_x, cursor_y))
         return display_list
 
@@ -101,6 +130,6 @@ class Browser:
         for c, x, y in self.display_list:
             if y > self.scroll + HEIGHT:
                 continue
-            if y + VSTEP < self.scroll:
+            if y + self.VSTEP < self.scroll:
                 continue
-            self.canvas.create_text(x, y - self.scroll, text=c)
+            self.canvas.create_text(x, y - self.scroll, text=c, font=self.font)
