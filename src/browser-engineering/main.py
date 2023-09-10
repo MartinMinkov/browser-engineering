2d5640fcfa91d59d074ee09b7e51b77506c2b999
Martin Minkov <minkovlmartin@gmail.com>
2023-09-10 15:01:05 -0700

    feat(main.py): add signal handler to gracefully exit on SIGINT
    This change allows the application to exit cleanly when the user interrupts the process (e.g., by pressing Ctrl+C).

--- src/browser-engineering/main.py
+++ src/browser-engineering/main.py
@@ -1,7 +1,17 @@
+import signal
+import sys
 import tkinter
 
 from src.render.browser import Browser
 from src.utils.url_factory import URLFactory
+
+
+def signal_handler(signal, frame):
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
 
 if __name__ == "__main__":
     import sys
